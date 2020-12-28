b39ebc4e90189bff941f57459c21e3b9f9922dcd
sophierahn <44484902+sophierahn@users.noreply.github.com>
2020-12-28 16:50:28 -0500

    Update SBUS.py
    Final Maybe

--- SBUS.py
+++ SBUS.py
@@ -56,6 +56,13 @@ class SBUS:
         self.time_to_land = False
         self.strange_packet = bytearray(_SBUS_PACKET_SIZE)
         self.num_strange_packets = 0
+        self.channels_in = [0]*_NUM_CHANNELS
+        self.opt_in = 0
+        self.packet_out = bytearray(_SBUS_PACKET_SIZE)
+        self.channels_out = [0]*16
+        self.opt_out = 0 
+        self.failed_packet = bytearray(_SBUS_PACKET_SIZE)
+        self.lost_packet = bytearray(_SBUS_PACKET_SIZE)
 
 
     def install(self): # set up the serial port, the object holds the access to the buffer i think
@@ -79,24 +86,24 @@ class SBUS:
         start_found = False
         x = 0
 
+        in_byte = uart.read(50)
+
 
         while x < 50:
             self.desync = False
-            
-            in_byte = uart.read(1)# this is taking the majority of the processing time
 
             self.SBUS_OK = True
 
-            if in_byte[0] == self._SBUS_HEADER and not start_found:
+            if in_byte[x] == self._SBUS_HEADER and not start_found:
                 start_found = True
-                self._packet[0] = in_byte[0]
+                self._packet[0] = in_byte[x]
                 _packetPos = 1
 
             #specifically elif so it doesn't copy in the start bit twice    
             elif start_found:
 
                 #start reading bytes into the _packet starting after the start byte
-                self._packet[_packetPos] = in_byte[0]
+                self._packet[_packetPos] = in_byte[x]
                 _packetPos += 1 
 
                 #if we've filled a full sbus _packet
@@ -105,25 +112,35 @@ class SBUS:
 
                     #check if its a valid packet
                     if self.verifyPacket():
-                        #give decodePacket a bytearray and return a list of channels and the bools
-                        channels_in, opt_in = self.decodePacket()
-                        #Mostly for beugging to catch the instances where the packets are still garbled at this point
-                        if channels_in[10] != 992:
+
+                        #give decodePacket a bytearray and it will update the member variables channels in and opt_in
+                        self.decodePacket()
+
+                        #Mostly for debugging to catch the instances where the packets are still garbled at this point
+
+                        if 124 in self.channels_in:
                             self.strange_packet = self._packet
                             self.num_strange_packets += 1
+                            if self.verifyPacket():
+                                print("Packet is still good")
+                            else:
+                                print("packet failed second check")
+                                break
+
 
                         if self.failSafe:
-                            self.time_to_land = True
-
-                        return channels_in, opt_in
+                            self.failed_packet = self._packet
+                        if self.frameLost:
+                            self.lost_packet = self._packet
+                        break
                     else:
             
                         self.desync = True
 
                         #mostly garbage data to indicate desync
-                        channels_in = [1,1,1,1,1800,1,1,1,1,1,1,1800,1,1,1,1]
-                        opt_in = 0b00100100 #ensure frame lost bit is set, could be position 2 or 6 depending on endian
-                        return channels_in, opt_in
+                        self.channels_in = [1,1,1,1,1800,1,1,1,1,1,1,1800,1,1,1,1]
+                        self.opt_in = 0b00100100 #ensure frame lost bit is set, could be position 2 or 6 depending on endian
+                        break
         #for loop incrementing            
             x += 1
                         
@@ -134,20 +151,24 @@ class SBUS:
     #takes in properly formatted packet list as well as serial port object
     #writes said packet to the serial port
     def write(self, packet, uart):
-
+        self.packet_out = bytearray(self._SBUS_PACKET_SIZE)
+        self.opt_out = 0
+        self.channels_out = [0]*self._NUM_CHANNELS
         
-        opt_out = 0
-        #take the packet tuple apart
-        channels_out = packet[0]
+        #take the packet list apart
+        self.channels_out = packet[0]
 
-        opt_out |= packet[1] * self.SBUS_OPT_C17 
-        opt_out |= packet[2] * self.SBUS_OPT_C18
-        opt_out |= packet[3] * self.SBUS_OPT_FS
-        opt_out |= packet[4] * self.SBUS_OPT_FL
+        self.opt_out |= packet[1] * self.SBUS_OPT_C17 
+        self.opt_out |= packet[2] * self.SBUS_OPT_C18
+        self.opt_out |= packet[3] * self.SBUS_OPT_FS
+        self.opt_out |= packet[4] * self.SBUS_OPT_FL
 
-        output = self.encode(channels_out, opt_out)
 
-        uart.write(output)
+        #uses member values channels_out and opt_out to build a different member array: packet_out
+        self.encode()
+
+        #encode fills the member array packet_out which can now be written to the serial port
+        uart.write(self.packet_out)
 
         
 
@@ -162,74 +183,50 @@ class SBUS:
         else:
             return False
     
-
-    #will take in a list of commands and put the in the byte array
-    #this function will vary as I adjust the PID library
-    #will output bytearray of channels
-    def fillChannels(self, commands):
-        channels = bytearray(commands)
-        return channels
-
     
     #A function to build a packet list and undergo some error checking
-    #maybe try ditching this function?
     def buildPacket(self, channels_out, ch17, ch18, failsafe, frameLost):
 
         #load up packet list
-        packet = channels_out, ch17, ch18, failsafe, frameLost
-        msg0 = ""
-        msg1 = ""
-        msg2 = ""
-        msg3 = ""
-        msg4 = ""
-        error = False
+        packet = [channels_out, ch17, ch18, failsafe, frameLost]
 
         #verify data types
         if not isinstance(packet[0], list):
             print(F"channels out is not a list")
-            error = True
 
         if not isinstance(packet[1], bool):
             print(F"ch17 is not a bool")
-            error = True
-
         if not isinstance(packet[2], bool):
             print(F"ch18 is not a bool")
-            error = True
         
         if not isinstance(packet[3], bool):
             print(F"failsafe is not a bool")
-            error = True
 
         if not isinstance(packet[4], bool):
             print(F"frameLost is not a bool")
-            error = True
         else:
             return packet
         
 
-
+#only ever one packet in and one packet out
     
     #Takes in channels list  and outputs a _packet bytearray
-    def encode(self, channels_out, opt_out):
-
-        #init stuff like packet array
-        packet = bytearray(self._SBUS_PACKET_SIZE)
+    def encode(self):
 
         #---------------------------
 
-        if not packet or not channels_out:
+        if not self.packet_out or not self.channels_out:
             print(F"something went wrong but frankly im not sure what")
+            #what even is this
 
         #---------------------------
-
         #set start and end bits
-        packet[0] = self._SBUS_HEADER
-        packet[24] = self._SBUS_END
+        self.packet_out[0] = self._SBUS_HEADER
+        self.packet_out[24] = self._SBUS_END
 
         #kick off packet loading alogrithm
-        packet[1] = channels_out[0] & 0xFF
-        packet[2] = channels_out[0] >> 8 & 0b111
+        self.packet_out[1] = self.channels_out[0] & 0xFF
+        self.packet_out[2] = self.channels_out[0] >> 8 & 0b111
         current_byte = 2
         usedBits = 3 # from LSB
 
@@ -240,7 +237,7 @@ class SBUS:
             while bitsWritten < 11:
 
                 #strip written bits, shift over used bits
-                packet[current_byte] |= channels_out[ch] >> bitsWritten << usedBits & 0xff
+                self.packet_out[current_byte] |= self.channels_out[ch] >> bitsWritten << usedBits & 0xFF
                 
                 hadToWrite = 11 - bitsWritten
                 couldWrite = 8 - usedBits
@@ -258,10 +255,7 @@ class SBUS:
             ch += 1
         
         #set error flags
-        packet[23] = opt_out
-
-        return packet
-
+        self.packet_out[23] = self.opt_out
 
 
 
@@ -273,41 +267,43 @@ class SBUS:
     #take in _packet and output channels list and opt
     #handles bit shifting and endian switch outlined for futuba SBUS
     def decodePacket(self):
-        channels_in = [0]*self._NUM_CHANNELS
 
 
-        channels_in[0] = (self._packet[1] | (self._packet[2] << 8)  & 0x07FF)
-        channels_in[1] = ((self._packet[2] >> 3) | (self._packet[3] << 5) & 0x07FF)
-        channels_in[2] = ((self._packet[3] >> 6) | (self._packet[4] << 2) | (self._packet[5] << 10) & 0x07FF)
-        channels_in[3] = ((self._packet[5] >> 1) | (self._packet[6] << 7) & 0x07FF)
-        channels_in[4] = ((self._packet[6] >> 4) | (self._packet[7] << 4) & 0x07FF)
-        channels_in[5] = ((self._packet[7] >> 7) | (self._packet[8] << 1) | (self._packet[9] << 9) & 0x07FF)
-        channels_in[6] = ((self._packet[9] >> 2) | (self._packet[10] << 6) & 0x07FF)
-        channels_in[7] = ((self._packet[10] >> 5) | (self._packet[11] << 3) & 0x07FF)
-        channels_in[8] = (self._packet[12]  | (self._packet[13] << 8) & 0x07FF)
-        channels_in[9] = ((self._packet[13] >> 3) | (self._packet[14] << 5) & 0x07FF)
-        channels_in[10] = ((self._packet[14] >> 6) | (self._packet[15] << 2) | (self._packet[16] << 10) & 0x07FF)
-        channels_in[11] = ((self._packet[16] >> 1) | (self._packet[17] << 7) & 0x07FF)
-        channels_in[12] = ((self._packet[17] >> 4) | (self._packet[18] << 4) & 0x07FF)
-        channels_in[13] = ((self._packet[18] >> 7) | (self._packet[19] << 1) | (self._packet[20] << 9) & 0x07FF)
-        channels_in[14] = ((self._packet[20] >> 2) | (self._packet[21] << 6) & 0x07FF)
-        channels_in[15] = ((self._packet[21] >> 5) | (self._packet[22] << 3) & 0x07FF)
+        self.channels_in[0] = (self._packet[1] | (self._packet[2] << 8)  & 0x07FF)
+        self.channels_in[1] = ((self._packet[2] >> 3) | (self._packet[3] << 5) & 0x07FF)
+        self.channels_in[2] = ((self._packet[3] >> 6) | (self._packet[4] << 2) | (self._packet[5] << 10) & 0x07FF)
+        self.channels_in[3] = ((self._packet[5] >> 1) | (self._packet[6] << 7) & 0x07FF)
+        self.channels_in[4] = ((self._packet[6] >> 4) | (self._packet[7] << 4) & 0x07FF)
+        self.channels_in[5] = ((self._packet[7] >> 7) | (self._packet[8] << 1) | (self._packet[9] << 9) & 0x07FF)
+        self.channels_in[6] = ((self._packet[9] >> 2) | (self._packet[10] << 6) & 0x07FF)
+        self.channels_in[7] = ((self._packet[10] >> 5) | (self._packet[11] << 3) & 0x07FF)
+        self.channels_in[8] = (self._packet[12]  | (self._packet[13] << 8) & 0x07FF)
+        self.channels_in[9] = ((self._packet[13] >> 3) | (self._packet[14] << 5) & 0x07FF)
+        self.channels_in[10] = ((self._packet[14] >> 6) | (self._packet[15] << 2) | (self._packet[16] << 10) & 0x07FF)
+        self.channels_in[11] = ((self._packet[16] >> 1) | (self._packet[17] << 7) & 0x07FF)
+        self.channels_in[12] = ((self._packet[17] >> 4) | (self._packet[18] << 4) & 0x07FF)
+        self.channels_in[13] = ((self._packet[18] >> 7) | (self._packet[19] << 1) | (self._packet[20] << 9) & 0x07FF)
+        self.channels_in[14] = ((self._packet[20] >> 2) | (self._packet[21] << 6) & 0x07FF)
+        self.channels_in[15] = ((self._packet[21] >> 5) | (self._packet[22] << 3) & 0x07FF)
 
-        opt = self._packet[23] & 0xF
+        self.opt_in = self._packet[23] & 0xF
 
 
         #create input packet list for passthrough situations
         #there will ALWAYS be a packet in and there will ALWAYS only be one (bc when new info comes in it gets updated)
         #so this will make it easily accessible for passthrough scenarios
-        self.ch17 = bool(opt & self.SBUS_OPT_C17)
-        self.ch18 = bool(opt & self.SBUS_OPT_C18)
-        self.failSafe = bool(opt & self.SBUS_OPT_FS)
-        self.frameLost = bool(opt & self.SBUS_OPT_FL)
+        self.ch17 = bool(self.opt_in & self.SBUS_OPT_C17)
+        self.ch18 = bool(self.opt_in & self.SBUS_OPT_C18)
+        self.failSafe = bool(self.opt_in & self.SBUS_OPT_FS)
+        self.frameLost = bool(self.opt_in & self.SBUS_OPT_FL)
 
-        self.packet_in = channels_in, self.ch17, self.ch18, self.failSafe, self.frameLost
+        self.packet_in = self.channels_in, self.ch17, self.ch18, self.failSafe, self.frameLost
         #dont need to worry about _packet[24] cause thats the end bit (0X00)
 
-        return channels_in, opt
+
+        #no need to return anything, it should update as a member of the class
+
+
 
 
 
